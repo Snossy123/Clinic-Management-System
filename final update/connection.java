53d7325f46999f615706a95246e642e4d96fcbb0
Snossy123 <61948065+Snossy123@users.noreply.github.com>
2021-01-08 01:58:32 +0200

    Add files via upload

---
+++ final update/connection.java
@@ -0,0 +1,32 @@
+package clinic;
+
+import com.mysql.jdbc.Connection;
+import java.sql.*;
+
+
+public class connection {
+   public String dbname = "clinic";
+   public String dbURL = "jdbc:mysql://localhost:3306/"+dbname; // conest 
+   public String username ="root"; 
+   public String password = "";  
+    
+    public  Connection conn() throws SQLException
+    {
+        Connection dbcon = null;
+    try
+    {
+        dbcon = (Connection) DriverManager.getConnection(this.dbURL, this.username, this.password);
+    }catch (SQLException se)
+     {
+         throw se;
+         
+     }
+     
+        return dbcon;
+  }
+//
+//    Statement createStatement() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//    
+}
